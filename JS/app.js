6b24960d8c2ddd22a9f1052fb68293da687412ef
namiren <namimatsuren@gmail.com>
2024-04-17 16:12:22 +0900

    changed the way to use vw

--- JS/app.js
+++ JS/app.js
@@ -38,3 +38,10 @@ function scrollTop(el, duration) {
         }
     });
 }
+
+const setVw = function () {
+    const vw = document.documentElement.clientWidth / 100;
+    document.documentElement.style.setProperty('--vw', `${vw}px`);
+}
+window.addEventListener('DOMContentLoaded', setVw);
+window.addEventListener('resize', setVw);
