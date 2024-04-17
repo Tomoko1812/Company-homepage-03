d72bc7f04216f4840bfacb2067ece1ed662804e4
GoodDot <141890030+GoodDoot@users.noreply.github.com>
2024-04-17 11:42:45 +0900

    Delete project directory

--- project/JS/service.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var contactButton = document.getElementById('contactButton');
-    contactButton.addEventListener('click', function () {
-        window.location.href = '/contact.html';
-    });
-});
